ae443447d7db59988697b5c80f6020547398ed9e
Rottin <rottinlew@163.com>
2017-05-13 12:26:06 +0800

    添加主页侧滑栏

--- app/src/main/java/com/rottin/administrator/pictag/MainActivity.java
+++ app/src/main/java/com/rottin/administrator/pictag/MainActivity.java
@@ -13,17 +13,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.nineoldandroids.view.ViewHelper;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+//        implements NavigationView.OnNavigationItemSelectedListener
+ {
     private Button bn1;
+    private DragLayout dl;
+    private ListView lv;
+    private ImageView iv_icon, iv_bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         bn1 = (Button) findViewById(R.id.bn1);
@@ -42,10 +56,59 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);*/
+        initDragLayout();
+        initView();
     }
 
-    @Override
+    private void initDragLayout() {
+        dl = (DragLayout) findViewById(R.id.dl);
+        dl.setDragListener(new DragLayout.DragListener() {
+            @Override
+            public void onOpen() {
+                lv.smoothScrollToPosition(new Random().nextInt(30));
+            }
+
+            @Override
+            public void onClose() {
+                shake();
+            }
+
+            @Override
+            public void onDrag(float percent) {
+                ViewHelper.setAlpha(iv_icon, 1 - percent);
+            }
+        });
+    }
+
+    private void shake() {
+        iv_icon.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake));
+    }
+
+    private void initView() {
+        iv_icon = (ImageView) findViewById(R.id.iv_icon);
+        iv_bottom = (ImageView) findViewById(R.id.iv_bottom);
+        lv = (ListView) findViewById(R.id.lv);
+        lv.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                R.layout.item_text, new String[] { "个人信息", "积分商城",
+                "历史记录", "反馈", "关于", "退出",
+        }));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int position, long arg3) {
+                Toast.makeText(getApplicationContext(), "click " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        iv_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                dl.open();
+            }
+        });
+    }
+
+    /*@Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -75,13 +138,13 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
 // TODO: 2017/5/10  
 //        if (id == R.id.nav_camera) {
 //            // Handle the camera action
@@ -96,9 +159,9 @@ public class MainActivity extends AppCompatActivity
 //        } else if (id == R.id.nav_send) {
 //
 //        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
 }
